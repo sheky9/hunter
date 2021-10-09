2ed275ce4bd02f09c38af34ac7cbcbdc16cfa254
Abhishek Soni <abhisheksoni099@gmail.com>
2021-10-09 17:30:02 +0530

    Initial commit

---
+++ src/test/java/com/abhisheksoni/hunter/HunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhisheksoni.hunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
